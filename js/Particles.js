ced8f482c8937d372a70986b15d2f5da28c13d34
Steven La <stevenla@g.ucla.edu>
2018-07-18 22:56:55 -0700

    added getBass, getTreble fn.s

--- js/Particles.js
+++ js/Particles.js
@@ -4,6 +4,7 @@ function ParticleObject(geometry, color, size) {
 
   this.mesh = particleGlobe;
   this.speed = 1;
+  this.invFlag = false;
 
   this.highlight = function(color) {
     this.mesh.material.color.r = color.r;
@@ -15,4 +16,30 @@ function ParticleObject(geometry, color, size) {
     this.mesh.rotation.y += 0.0025*this.speed;
     this.mesh.rotation.z += 0.0025*this.speed;
   }
+}
+
+function getBass(listener, analyser) {
+    var freqPerBin = listener.context.sampleRate / 32;
+    var length = 350 / freqPerBin;
+    var values = 0;
+    var average;
+    var arr = analyser.getFrequencyData();
+    for(var i = 0; i < length; i++){
+      values += arr[i];
+    }
+    average = values / length;
+    return average;
+}
+
+function getTreble(listener, analyser) {
+    var freqPerBin = listener.context.sampleRate / 32;
+    var start = Math.floor(3000 / freqPerBin);
+    var values = 0;
+    var average;
+    var arr = analyser.getFrequencyData();
+    for(var i = start; i < 16; i++){
+      values += arr[i];
+    }
+    average = values / (16 - start);
+    return average;
 }
